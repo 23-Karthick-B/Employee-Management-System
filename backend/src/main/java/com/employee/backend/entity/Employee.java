8a922225f90892f65e2da2d0c44d69befc8a71ea
Karthick <officialkarthick23@gmail.com>
2026-04-10 14:48:59 +0530

    Removed the DB part just basic API

--- backend/src/main/java/com/employee/backend/entity/Employee.java
+++ backend/src/main/java/com/employee/backend/entity/Employee.java
@@ -1,117 +1,117 @@
-package com.employee.backend.entity;
+// package com.employee.backend.entity;
 
-import java.util.Date;
+// import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "employee")
-public class Employee {
-    @Id
-    @GeneratedValue(strategy =GenerationType.IDENTITY)
-    private Integer id;
+// @Entity
+// @Table(name = "employee")
+// public class Employee {
+//     @Id
+//     @GeneratedValue(strategy =GenerationType.IDENTITY)
+//     private Integer id;
 
-    @Column(nullable = false)
-    private String fullName;
+//     @Column(nullable = false)
+//     private String fullName;
 
-    @Column(nullable = false)
-    private String gender;
+//     @Column(nullable = false)
+//     private String gender;
 
-    @Column(nullable = false,unique = true)
-    private String email;
+//     @Column(nullable = false,unique = true)
+//     private String email;
 
-    @Column(nullable = false)
-    private String department;
+//     @Column(nullable = false)
+//     private String department;
 
-    @Column(nullable = false)
-    private String role;
+//     @Column(nullable = false)
+//     private String role;
 
-    @Column(nullable = false)
-    private Date dob;
+//     @Column(nullable = false)
+//     private Date dob;
 
-    @Column(nullable = false,unique = true)
-    private String phoneNumber;
+//     @Column(nullable = false,unique = true)
+//     private String phoneNumber;
 
-    public Integer getId() {
-        return id;
-    }
+//     public Integer getId() {
+//         return id;
+//     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+//     public void setId(Integer id) {
+//         this.id = id;
+//     }
 
-    public String getFullName() {
-        return fullName;
-    }
+//     public String getFullName() {
+//         return fullName;
+//     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
+//     public void setFullName(String fullName) {
+//         this.fullName = fullName;
+//     }
 
-    public String getGender() {
-        return gender;
-    }
+//     public String getGender() {
+//         return gender;
+//     }
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
+//     public void setGender(String gender) {
+//         this.gender = gender;
+//     }
 
-    public String getEmail() {
-        return email;
-    }
+//     public String getEmail() {
+//         return email;
+//     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
 
-    public String getDepartment() {
-        return department;
-    }
+//     public String getDepartment() {
+//         return department;
+//     }
 
-    public void setDepartment(String department) {
-        this.department = department;
-    }
+//     public void setDepartment(String department) {
+//         this.department = department;
+//     }
 
-    public String getRole() {
-        return role;
-    }
+//     public String getRole() {
+//         return role;
+//     }
 
-    public void setRole(String role) {
-        this.role = role;
-    }
+//     public void setRole(String role) {
+//         this.role = role;
+//     }
 
-    public Date getDob() {
-        return dob;
-    }
+//     public Date getDob() {
+//         return dob;
+//     }
 
-    public void setDob(Date dob) {
-        this.dob = dob;
-    }
+//     public void setDob(Date dob) {
+//         this.dob = dob;
+//     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+//     public String getPhoneNumber() {
+//         return phoneNumber;
+//     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+//     public void setPhoneNumber(String phoneNumber) {
+//         this.phoneNumber = phoneNumber;
+//     }
 
-    public Employee(Integer id, String fullName, String gender, String email, String department, String role, Date dob,
-            String phoneNumber) {
-        this.id = id;
-        this.fullName = fullName;
-        this.gender = gender;
-        this.email = email;
-        this.department = department;
-        this.role = role;
-        this.dob = dob;
-        this.phoneNumber = phoneNumber;
-    }
+//     public Employee(Integer id, String fullName, String gender, String email, String department, String role, Date dob,
+//             String phoneNumber) {
+//         this.id = id;
+//         this.fullName = fullName;
+//         this.gender = gender;
+//         this.email = email;
+//         this.department = department;
+//         this.role = role;
+//         this.dob = dob;
+//         this.phoneNumber = phoneNumber;
+//     }
 
     
-}
+// }
