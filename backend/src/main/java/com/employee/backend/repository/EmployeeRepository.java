9ad795e530bc8551c797a0c8df097a2f53d35b5a
Karthick <officialkarthick23@gmail.com>
2026-04-22 14:59:06 +0530

    Created Entity and DB Connection

--- backend/src/main/java/com/employee/backend/repository/EmployeeRepository.java
+++ backend/src/main/java/com/employee/backend/repository/EmployeeRepository.java
@@ -1,5 +1,12 @@
 package com.employee.backend.repository;
 
-public class EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.employee.backend.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
+
+    boolean existsByEmailIgnoreCase(String email);
+    boolean existsByEmailIgnoreCaseAndIdNot(String email, Integer id);
+    
 }
