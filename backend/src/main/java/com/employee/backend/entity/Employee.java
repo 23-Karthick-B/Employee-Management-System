304725288a1be4594ad6401e99eaa9ffc8ee47ab
Karthick <officialkarthick23@gmail.com>
2026-04-22 16:24:34 +0530

    Entity validation done

--- backend/src/main/java/com/employee/backend/entity/Employee.java
+++ backend/src/main/java/com/employee/backend/entity/Employee.java
@@ -8,7 +8,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "Employees")
@@ -18,30 +22,48 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Id")
     private Integer id;
-
+    
+    @NotBlank(message = "Name is required")
     @Column(name = "Name", nullable = false)
     private String name;
 
+    @NotBlank(message = "Email is required")
     @Column(name = "Email", nullable = false)
     private String email;
 
+    @NotBlank(message = "Department is required")
     @Column(name = "Department", nullable = false)
     private String department;
 
+    @NotBlank(message = "PhoneNumber is required")
     @Column(name = "PhoneNumber", nullable = false)
     private String phoneNumber;
 
+    @NotNull(message = "DOB is required")
     @Column(name = "DOD", nullable = false)
     private LocalDate dod; 
 
     @Column(name = "IsActive")
-    private Boolean isActive;
+    private Boolean isActive = true;
 
     @Column(name = "CreatedAt", updatable = false)
     private LocalDateTime createdAt;
 
     @Column(name = "UpdatedAt")
     private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
 
     public Integer getId() {
         return id;
